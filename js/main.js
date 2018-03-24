abef8daaf548c8276abdb3be41c214908f1e492a
Nigel C <cheok.nigel@gmail.com>
2018-03-24 12:46:18 +0800

    added padding to centre

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'An image from ' + restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
