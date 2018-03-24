3661c8d7ccaf72322b92cd8a955aa8be05616fd5
Nigel C <cheok.nigel@gmail.com>
2018-03-24 21:21:36 +0800

    added meta tag for viewport and aria role

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'An image from ' + restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
